e77bf4a95fbd948ce1887a5d6f8e05ce0c30da65
Erling <erling.jellum@gmail.com>
2021-04-28 11:22:25 +0200

    Add tests that catches TesterMemoryWrapper bug

--- src/test/scala/TestTesterMemoryWrapper.scala
+++ src/test/scala/TestTesterMemoryWrapper.scala
@@ -99,7 +99,6 @@ class TestTesterMemoryWrapper extends FlatSpec with ChiselScalatestTester with M
         val d  = Seq.tabulate(8)(j =>i*8 + j)
         writeMem(c, addr = i*64, numBytes = 64, data = d)
       }
-
       for (i <- 0 until 32) {
         if (i < 16) {
           expectRead(c, addr = i*8, numBytes = 8, data = Seq(data(i)))
@@ -112,4 +111,45 @@ class TestTesterMemoryWrapper extends FlatSpec with ChiselScalatestTester with M
       }
     }
   }
+  it should "work with stalling" in {
+
+    test(new TesterMemoryWrapper(TesterWrapperParams, numMemPorts = 1)) { c =>
+      initClocks(c)
+
+      val data = Seq.tabulate(8)(i => i)
+      writeMem(c, addr = 0, numBytes = 64, data = data)
+      c.accio.memPort(0).memRdReq.enqueue(chiselTypeOf(c.accio.memPort(0).memRdReq).bits.Lit(
+        _.addr -> 0.U,
+        _.metaData -> 0.U,
+        _.channelID -> 0.U,
+        _.numBytes -> 64.U,
+        _.isWrite -> false.B
+      )
+      )
+      val dsplit = data.splitAt(4)
+
+
+
+      c.accio.memPort(0).memRdRsp.expectDequeueSeq(dsplit._1.zipWithIndex.map(d => {
+        chiselTypeOf(c.accio.memPort(0).memRdRsp).bits.Lit(
+          _.readData -> d._1.U,
+          _.isLast ->  (d._2 == data.size-1).B,
+          _.channelID -> 0.U,
+          _.isWrite -> false.B,
+          _.metaData -> 0.U
+        )}))
+
+      c.clock.step(4)
+
+      c.accio.memPort(0).memRdRsp.expectDequeueSeq(dsplit._2.zipWithIndex.map(d => {
+        chiselTypeOf(c.accio.memPort(0).memRdRsp).bits.Lit(
+          _.readData -> d._1.U,
+          _.isLast ->  (d._2 == data.size-1).B,
+          _.channelID -> 0.U,
+          _.isWrite -> false.B,
+          _.metaData -> 0.U
+        )}))
+
+      }
+    }
 }
